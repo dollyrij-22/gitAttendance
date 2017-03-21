e861a82e36e58a1474aa3ad42fda9301a98bf17a
Dolly Rijwani <dollyrijwani22@gmail.com>
2017-03-20 18:17:27 -0700

    location

--- location.php
+++ location.php
@@ -8,5 +8,12 @@ $date =$_POST["date"];
 
 
 $sql = "INSERT INTO location(username, longitude, latitude, date) VALUES ('$username', '$longitude', '$latitude', '$date')";
+if(mysqli_query($con, $sql)){
+	echo 'location inserted successfully';
+}
+else
+{
+	echo 'Data insertion error!!!'.mysqli_error($con);
+}
 
 ?>
